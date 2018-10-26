88e4e7528cea26c6923e1cbac94773d131abd9ba
djovanov92 <djovanov92@gmail.com>
2018-10-26 22:48:50 +0200

    authentication, routing guards and autologin with tokens

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -9,7 +9,7 @@ class Modal extends Component {
     return nextProps.show !== this.props.show || nextProps.children !== this.props.children;
   }
 
-  componentWillUpdate = (nextProps, nextState) => {
+  componentWillUpdate = () => {
     console.log("[Modal] WillUpdate");
   }
   
